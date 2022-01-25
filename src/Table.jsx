51b736804217103dc747a8350c6a72133c387998
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-01-25 16:41:36 -0300

    Requisito 2

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,47 +1,59 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import PlanetsContext from './context';
 
 function Table() {
   const data = useContext(PlanetsContext);
+  const [filter, setFilter] = useState('');
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Rotation Period</th>
-          <th>Orbital Period</th>
-          <th>Diameter</th>
-          <th>Climate</th>
-          <th>Gravity</th>
-          <th>Terrain</th>
-          <th>Surface Water</th>
-          <th>Population</th>
-          <th>Films</th>
-          <th>Created</th>
-          <th>Edited</th>
-          <th>URL</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item, index) => (
-          <tr key={ index }>
-            <td>{ item.name }</td>
-            <td>{ item.rotation_period }</td>
-            <td>{ item.orbital_period }</td>
-            <td>{ item.diameter }</td>
-            <td>{ item.climate }</td>
-            <td>{ item.gravity }</td>
-            <td>{ item.terrain }</td>
-            <td>{ item.surface_water }</td>
-            <td>{ item.population }</td>
-            <td>{ item.films }</td>
-            <td>{ item.created }</td>
-            <td>{ item.edited }</td>
-            <td>{ item.url }</td>
+    <>
+      <input
+        type="text"
+        data-testid="name-filter"
+        placeholder="Filtro por nome"
+        value={ filter }
+        onChange={ (event) => setFilter(event.target.value) }
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data
+            .filter((dat) => dat.name.includes(filter))
+            .map((item, index) => (
+              <tr key={ index }>
+                <td>{item.name}</td>
+                <td>{item.rotation_period}</td>
+                <td>{item.orbital_period}</td>
+                <td>{item.diameter}</td>
+                <td>{item.climate}</td>
+                <td>{item.gravity}</td>
+                <td>{item.terrain}</td>
+                <td>{item.surface_water}</td>
+                <td>{item.population}</td>
+                <td>{item.films}</td>
+                <td>{item.created}</td>
+                <td>{item.edited}</td>
+                <td>{item.url}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
