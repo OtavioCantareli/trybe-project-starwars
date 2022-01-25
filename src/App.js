00e3e58876ca5dde46e67b9620c3bffba88f2582
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-01-25 15:59:43 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import PlanetsContext from './context';
+import Table from './Table';
+
+const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const results = await fetch(url).then((response) => response
+        .json()
+        .then((resp) => resp.results)
+        .catch((error) => error));
+      setData(results);
+    }
+    fetchData();
+  }, []);
+
   return (
-    <span>Hello, App!</span>
+    <PlanetsContext.Provider value={ data }>
+      <Table />
+    </PlanetsContext.Provider>
   );
 }
-// commit!
+
 export default App;
